b6c9137866432c06c69a57ec874bc2cf237527e7
Rezontov Alexandr <sasarezontov649@gmail.com>
2024-02-06 15:19:04 +0300

    form3

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-from db import DatabaseManager
+from db import DBmanager
+from config import *
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
+@app.route("/test")
+def test():
+    return render_template('reg.html')
+@app.route("/registration")
+def registration():
+    return render_template('form.html')
 
-app.route("/registration")
-def admin():
-    return render_template('admin.html')
+@app.route("/read_form", methods=['POST'])
+def read_form():
+    base = DBmanager(host,user,password,name)
+    self.qery('''CREATE TABLE IF NOT EXISTS Events(name text, date date, time text, members text)''')
+    data=request.form
+    event = text['event']
+    date = data['date']
+    team = text['team']
+    members = text['members']
+    dictsend = (event, date, team, members)
+    users.query('''INSERT INTO Events VALUES (?,?,?,?)''',dictsend)
+    return render_template('read_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route("/read_form", methods=['POST'])
-def read_form():
-    users = DatabaseManager('users.db')
-    users.create_tables()
-    data=request.form
-    userEmail = data['userEmail']
-    userNmae = data['name']
-    userLastname = data['lastname']
-    dictsend = (userEmail, userNmae, userLastname)
-    users.query('''INSERT INTO Users VALUES (?,?,?)''',dictsend)
-    return render_template('read_form.html')
