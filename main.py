d48ae8a16d71d8a3e0be92c40ca3d85eae44f044
Rezontov Alexandr <sasarezontov649@gmail.com>
2024-02-13 14:30:08 +0300

    regcomplit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from db import DBmanager
+from dbmodel import DBmanager
 from config import *
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,14 +16,14 @@ def registration():
 @app.route("/read_form", methods=['POST'])
 def read_form():
     base = DBmanager(host,user,password,name)
-    self.qery('''CREATE TABLE IF NOT EXISTS Events(name text, date date, time text, members text)''')
-    data=request.form
-    event = text['event']
+    base.query('''CREATE TABLE IF NOT EXISTS Events(name text, date date, team text, members text)''')
+    data = request.form
+    eventname = data['eventName']
     date = data['date']
-    team = text['team']
-    members = text['members']
-    dictsend = (event, date, team, members)
-    users.query('''INSERT INTO Events VALUES (?,?,?,?)''',dictsend)
+    team = data['team']
+    members = data['members']
+    dictsend = (eventname, date, team, members)
+    base.query('''INSERT INTO Events(name, date, team, members) VALUES (%s, %s, %s, %s)''', dictsend)
     return render_template('read_form.html')
 
 if __name__ == '__main__':
