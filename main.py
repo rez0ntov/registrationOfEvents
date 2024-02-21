80b04f79f1400dc68c0b37ca3bee5814e42e1b29
Sasha Rezontov <sasarezontov649@gmail.com>
2024-02-22 00:45:18 +0300

    list

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import mysql.connector
 from dbmodel import DBmanager
 from config import *
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
-
 @app.route("/")
 def admin_login():
     return render_template('admin_login.html')
@@ -12,7 +11,17 @@ def form():
     return render_template('form.html')
 @app.route("/eventslist")
 def eventslist():
-    return render_template('eventslist.html')
+    myconn = mysql.connector.connect(host="mysql94.1gb.ru", user="gb_regkv", password="KHKFrmM-85pK", database="gb_regkv")
+    cur=myconn.cursor()
+    try:
+        cur.execute('SELECT * FROM Events')
+        result = cur.fetchall()
+        for x in result:
+            print(x)
+    except:
+        myconn.rollback()
+    myconn.close()
+    return render_template('eventslist.html',result=result)
 
 @app.route("/read_form", methods=['POST'])
 def read_form():
