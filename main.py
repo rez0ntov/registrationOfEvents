de2d1374e7cf05e44bc0ea999125e01b18877cd6
Rezontov Alexandr <sasarezontov649@gmail.com>
2024-02-27 15:06:52 +0300

    newlist

--- main.py
+++ main.py
@@ -11,29 +11,25 @@ def form():
     return render_template('form.html')
 @app.route("/eventslist")
 def eventslist():
-    myconn = mysql.connector.connect(host="mysql94.1gb.ru", user="gb_regkv", password="KHKFrmM-85pK", database="gb_regkv")
-    cur=myconn.cursor()
+    base = DBmanager(host, user, password, name)
     try:
-        cur.execute('SELECT * FROM Events')
-        result = cur.fetchall()
+        result = base.fetchall('SELECT * FROM Events')
         for x in result:
             print(x)
     except:
-        myconn.rollback()
-    myconn.close()
+        print('error')
     return render_template('eventslist.html',result=result)
 
 @app.route("/read_form", methods=['POST'])
 def read_form():
     base = DBmanager(host,user,password,name)
-    base.query('''CREATE TABLE IF NOT EXISTS Events(name text, date date, team text, members text)''')
+    base.query('''CREATE TABLE IF NOT EXISTS Events(name text, date date, team text)''')
     data = request.form
     eventname = data['eventName']
     date = data['date']
     team = data['team']
-    members = data['members']
-    dictsend = (eventname, date, team, members)
-    base.query('''INSERT INTO Events(name, date, team, members) VALUES (%s, %s, %s, %s)''', dictsend)
+    dictsend = (eventname, date, team)
+    base.query('''INSERT INTO Events(name, date, team) VALUES (%s, %s, %s)''', dictsend)
     return render_template('read_form.html')
 
 if __name__ == '__main__':
