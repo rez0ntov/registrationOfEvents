3f86bd9aa5779bbd36478cbd9db7ecd0ec89bd4e
Sasha Rezontov <sasarezontov649@gmail.com>
2024-03-20 00:52:21 +0300

    login and list

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ login_manager.login_message_category = "success"
 app.secret_key = SECRET_KEY
 
 
+# !!! email: adminemail11@gmail.com password: adminpasswd !!! #
+
 @login_manager.user_loader
 def load_user(user_id):
     dbase = DBmanager(host="mysql94.1gb.ru", user="gb_regkv", passwd="KHKFrmM-85pK", name="gb_regkv")
@@ -20,11 +22,59 @@ def load_user(user_id):
     return UserLogin().fromDB(int(user_id), dbase)
 
 
+# @app.route("/login", methods=["POST","GET"])
+# def login():
+#     dbase = DBmanager(host="mysql94.1gb.ru", user="gb_regkv", passwd="KHKFrmM-85pK", name="gb_regkv")
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#
+#     if request.method == "POST":
+#         user = dbase.getUserByEmail(request.form['email'])
+#         if user and check_password_hash(user[3], request.form['psw']):
+#             userlogin = UserLogin().create(user)
+#             rm = True if request.form.get('remainme') else False
+#             login_user(userlogin, remember=rm)
+#             return redirect( url_for("login"))
+#
+#         flash("Неверная пара логин/пароль", "error")
+#
+#     return render_template("login.html")
+
+
+@app.route('/log')
+def log():
+    return render_template('login.html')
+
+
+@app.route("/register", methods=["POST","GET"])
+def register():
+    dbase = DBmanager(host="mysql94.1gb.ru", user="gb_regkv", passwd="KHKFrmM-85pK", name="gb_regkv")
+    print(request.form['psw'])
+    hash = generate_password_hash(request.form['psw'])
+    res = dbase.addUser(request.form['name'], request.form['email'], hash)
+    print(res)
+    return render_template("register.html")
+
+
+@app.route('/reg')
+def reg():
+    return render_template('reg.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+
+@app.route("/")
 @app.route("/login", methods=["POST","GET"])
 def login():
     dbase = DBmanager(host="mysql94.1gb.ru", user="gb_regkv", passwd="KHKFrmM-85pK", name="gb_regkv")
     if current_user.is_authenticated:
-        return redirect(url_for('admin_login'))
+        return redirect(url_for('index'))
 
     if request.method == "POST":
         user = dbase.getUserByEmail(request.form['email'])
@@ -37,48 +87,17 @@ def login():
         flash("Неверная пара логин/пароль", "error")
 
     return render_template("login.html")
-
-
-@app.route('/log')
-def reg():
-    return render_template('login.html')
-
-#
-# @app.route("/register", methods=["POST","GET"])
-# def register():
-#     dbase = DBmanager(host="mysql94.1gb.ru", user="gb_regkv", passwd="KHKFrmM-85pK", name="gb_regkv")
-#     print(request.form['psw'])
-#     hash = generate_password_hash(request.form['psw'])
-#     res = dbase.addUser(request.form['name'], request.form['email'], hash)
-#     print(res)
-#     return render_template("register.html")
-#
-#
-# @app.route('/reg')
-# def reg():
-#     return render_template('reg.html')
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash("Вы вышли из аккаунта", "success")
-    return redirect(url_for('login'))
-
-
-@app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html')
 
 
-# @app.route("/form")
-# def form():
-#     return render_template('form.html')
+@app.route("/form")
+def form():
+    return render_template('form.html')
 
 @app.route("/createevent")
-def form():
+def createevent():
     return render_template('createevent.html')
 
 
@@ -95,17 +114,17 @@ def eventslist():
     return render_template('eventslist.html',result=result)
 
 
-# @app.route("/read_form", methods=['POST'])
-# def read_form():
-#     base = DBmanager(host,user,password,name)
-#     base.query('''CREATE TABLE IF NOT EXISTS Events(name text, date date, team text)''')
-#     data = request.form
-#     eventname = data['eventName']
-#     date = data['date']
-#     team = data['team']
-#     dictsend = (eventname, date, team)
-#     base.query('''INSERT INTO Events(name, date, team) VALUES (%s, %s, %s)''', dictsend)
-#     return render_template('read_form.html')
+@app.route("/read_form", methods=['POST'])
+def read_form():
+    base = DBmanager(host,user,password,name)
+    base.query('''CREATE TABLE IF NOT EXISTS Events(name text, date date, team text)''')
+    data = request.form
+    eventname = data['eventName']
+    date = data['date']
+    team = data['team']
+    dictsend = (eventname, date, team)
+    base.query('''INSERT INTO Events(name, date, team) VALUES (%s, %s, %s)''', dictsend)
+    return render_template('read_form.html')
 
 @app.route("/read_createevent", methods=['POST'])
 def read_createevent():
